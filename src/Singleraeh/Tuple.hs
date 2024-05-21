8c3382dfe066d8878b189eb873ebc385ae68c23d
Ben Orchard <thefirstmuffinman@gmail.com>
2024-05-21 15:40:06 +0100

    Tuple: +demoter

--- src/Singleraeh/Tuple.hs
+++ src/Singleraeh/Tuple.hs
@@ -5,3 +5,11 @@ import Data.Kind ( Type )
 type STuple2 :: (a -> Type) -> (b -> Type) -> (a, b) -> Type
 data STuple2 sa sb ab where
     STuple2 :: sa a -> sb b -> STuple2 sa sb '(a, b)
+
+demoteSTuple2
+    :: forall da db sa sb ab
+    .  (forall a. sa a -> da)
+    -> (forall b. sb b -> db)
+    -> STuple2 sa sb ab
+    -> (da, db)
+demoteSTuple2 demoteSA demoteSB (STuple2 sa sb) = (demoteSA sa, demoteSB sb)
