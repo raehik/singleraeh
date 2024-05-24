eb043a6c7a9fc9343d4218fc08e4cc73deb8a3c8
Ben Orchard <thefirstmuffinman@gmail.com>
2024-05-24 20:53:15 +0100

    Natural: +fixity definitions

--- src/Singleraeh/Natural.hs
+++ src/Singleraeh/Natural.hs
@@ -3,14 +3,17 @@ module Singleraeh.Natural where
 import GHC.TypeNats
 import Unsafe.Coerce ( unsafeCoerce )
 
+infixl 6 %+
 (%+) :: SNat n -> SNat m -> SNat (n + m)
 n %+ m = withSomeSNat (fromSNat n + fromSNat m) unsafeCoerce
 
-(%*) :: SNat n -> SNat m -> SNat (n * m)
-n %* m = withSomeSNat (fromSNat n * fromSNat m) unsafeCoerce
-
+infixl 6 %-
 (%-) :: SNat n -> SNat m -> SNat (n - m)
 n %- m = withSomeSNat (fromSNat n - fromSNat m) unsafeCoerce
+
+infixl 7 %*
+(%*) :: SNat n -> SNat m -> SNat (n * m)
+n %* m = withSomeSNat (fromSNat n * fromSNat m) unsafeCoerce
 
 sMod :: SNat n -> SNat m -> SNat (Mod n m)
 sMod n m = withSomeSNat (mod (fromSNat n) (fromSNat m)) unsafeCoerce
